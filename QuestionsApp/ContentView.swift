3955016fb41be1180e503197e5fbab3f1c73aa78
Lilit <martirosianli14@gmail.com>
2024-11-22 13:28:36 +0400

    Initial Commit

---
+++ QuestionsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuestionsApp
+//
+//  Created by Mac User on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuestionsApp/QuestionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionsAppApp.swift
+//  QuestionsApp
+//
+//  Created by Mac User on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
